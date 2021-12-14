b35afe9b31c5e3844682064a144a52e37e168ac7
NutNatcha <natcha_me@cmu.ac.th>
2021-12-14 17:21:07 +0700

    lab10_1 complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,11 +2,16 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double loan, rate, pay, interest, total, newBalance;
+    int year = 0;
 	cout << "Enter initial loan: ";
+    cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+    cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
+    cin >> pay;
+    
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -18,16 +23,33 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+	do{
+        year += 1;
+        loan = loan;
+        interest = loan*(rate/100);
+        total = loan + interest;
+        if(total > pay){
+            newBalance = total - pay;
+        }else if(total < pay){
+            pay = total;
+            newBalance = total - total;
+        }
+
+        cout << fixed << setprecision(2);
+        cout << setw(13) << left << year;
+        cout << setw(13) << left << loan;
+        cout << setw(13) << left << interest;
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << pay;
+        cout << setw(13) << left << newBalance;
+        cout << "\n";
+
+        if(newBalance > 0){
+            loan = newBalance;
+        }else if(newBalance == 0){
+            break;
+        }
+    }
+    while(1);
+    return 0;
 }
